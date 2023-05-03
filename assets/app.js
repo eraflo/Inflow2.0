2e1f4a375797b7084ba8cb6ddce37c6090953ce6
Florian Maillard <81640753+eraflo@users.noreply.github.com>
2023-05-03 22:54:13 +0200

    début mise en page
    ajout du header, avec un peu de style (pas fini) + création page d'accueil avec header, mais pas les pages de login et d'inscription

--- assets/app.js
+++ assets/app.js
@@ -6,7 +6,7 @@
  */
 
 // any CSS you import will output into a single css file (app.css in this case)
-import './styles/app.css';
+import './styles/app.scss';
 
 // start the Stimulus application
 import './bootstrap';
@@ -14,4 +14,7 @@ import './bootstrap';
 //img
 import './img/all/logo.png';
 import './img/all/banner.png';
+
+require('@fortawesome/fontawesome-free/css/all.min.css');
+require('@fortawesome/fontawesome-free/js/all.js');
 
