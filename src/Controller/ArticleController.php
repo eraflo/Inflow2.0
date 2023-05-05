63489b0e6aa5a1e359f14769bd399fe6d2af4561
Florian Maillard <81640753+eraflo@users.noreply.github.com>
2023-05-06 00:40:32 +0200

    controller : ajout de la méthode pour afficher 1 article, et de celle pour afficher tous les articles

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -2,17 +2,30 @@
 
 namespace App\Controller;
 
+use App\Repository\ArticlesRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class ArticleController extends AbstractController
 {
-    #[Route('/article', name: 'app_article')]
-    public function index(): Response
+    #[Route('/articles/{id}', name: 'app_article', requirements:['id'=>'\d+'], defaults:['id'=>1], methods: ['GET'])]
+    public function index($id, ArticlesRepository $repo): Response
     {
+        $article = $repo->find($id);
+
         return $this->render('article/index.html.twig', [
-            'controller_name' => 'ArticleController',
+            'article' => $article,
+        ]);
+    }
+
+    #[Route('/articles', name: 'app_articles', methods: ['GET'])]
+    public function articles(ArticlesRepository $repo): Response
+    {
+        $articles = $repo->findAll();
+
+        return $this->render('article/articles.html.twig', [
+            'articles' => $articles,
         ]);
     }
 }
