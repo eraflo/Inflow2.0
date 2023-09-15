56a9f74949b280dbfedfb36ec3cd7721f9f4bbfd
WEAKNEEESSDOTWAV <weakneeess.wav@gmail.com>
2023-09-15 15:38:34 +0200

    working mic

--- assets/js/mic.js
+++ assets/js/mic.js
@@ -104,35 +104,35 @@ function animate() {
   t += 1 / 60;
   coef = (coef > 0) ? Math.exp(-1 / 10 * t) * (1 + 1 / 10 * coef) : 0;
 
-  /* for (let timeValue of timeValues) {
+  for (let timeValue of timeValues) {
     let add = (Math.exp(-1/(4*(t - timeValue)))+Math.log((-(t - timeValue)+20)/20));
     if (add <= -0.01) {
       valuesToEvict.push(timeValue);
     } else {
       coef += add;
     }
-  } */
+  }
 
-  let add;
+  /* let add;
 
   for (let timeValue of timeValues) {
     if (t - timeValue >= 10) {
-      add = (Math.exp(-1/(4*(t - timeValue)))+Math.log((-(t - timeValue)+20)/20));
+      add = (Math.exp(-1 / (4 * (t - timeValue))) + Math.log((-(t - timeValue) + 20) / 20));
     } else {
-      add = (Math.exp(-1/(4*(t - timeValue)))+Math.log((-(t - timeValue)+20)/20));
+      add = (Math.exp(-1 / (4 * (t - timeValue))) + Math.log((-(t - timeValue) + 20) / 20));
     }
     coef += add;
-  }
+  } */
 
-  /* for (let valueToEvict of valuesToEvict) {
+  for (let valueToEvict of valuesToEvict) {
     timeValues.splice(timeValues.indexOf(valueToEvict), 1);
   }
-  valuesToEvict = []; */
+  valuesToEvict = [];
   ZMicRotation = - teta0 * Math.sin(Math.sqrt(9.81 / 2) * t) * (coef) + Math.PI;
   mic.rotation.z = ZMicRotation;
   mic.position.x = - 4 * Math.sin(mic.rotation.z);
   mic.position.y = 4 * Math.cos(mic.rotation.z);
-  
+
   renderer.render(scene, camera);
   requestAnimationFrame(animate);
 }
